d19fb8ba47dde6e718ff3f67a7a0f252c46e5c2e
TylerWang3892 <54153292+TylerWang3892@users.noreply.github.com>
2019-09-27 13:51:31 -0500

    First

---
+++ Chapter3B Starter Code/e03/AdjacentDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AdjacentDuplicates
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+
+
+      // Find and print adjacent duplicates
+
+   }
+}
