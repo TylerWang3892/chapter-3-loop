45eb33e6702e15affa86c60320f9a5a458f288c6
Tyler Wang <wangtyler123@gmail.com>
2019-10-06 20:34:03 -0500

    finished solo practice labs

---
+++ Chapter3B Starter Code/soloLabs/CSU_problem.java
@@ -0,0 +1,54 @@
+package soloLabs;
+
+import java.util.Scanner;
+
+public class CSU_problem
+{
+    public static void main()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        int number = keyboard.nextInt();
+        int count = 0;
+        int nextLine = 0;
+        while(number != 1)
+            if(number % 2 == 0)
+            {
+                number /= 2;
+                if(nextLine > 50)
+                {
+                    System.out.println();
+                    nextLine = 0;
+                }
+                System.out.print(number + " ");
+                nextLine += 1 + numDig(number);
+                count++;
+            }
+            else
+            {
+                number = number * 3 + 1;
+                if(nextLine > 50)
+                {
+                    System.out.println();
+                    nextLine = 0;
+                }
+                System.out.print(number +  " ");
+                nextLine += 1 + numDig(number);
+                count++;
+            }
+        System.out.println("\n");
+            
+        System.out.println("There are: " + count + " members in the set");
+    }
+    static int numDig(int a_number)
+    {
+        int number = a_number;
+        int numofDig = 0;
+        while(number != 0)
+        {
+            number /= 10;
+            numofDig++;
+        }
+        return numofDig;
+    }
+}
--- Chapter3B Starter Code/soloLabs/subString.java
+++ Chapter3B Starter Code/soloLabs/subString.java
@@ -1,6 +1,19 @@
 package soloLabs;
 
+import java.util.Scanner;
+
 public class subString
 {
-    
+    public static void main()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        String word = keyboard.nextLine();
+        
+        for(int a = 0; a < word.length(); a++)
+        {
+            for(int b = a; b <= word.length(); b++)
+                System.out.println(word.substring(a,b));
+        }
+    }
 }
