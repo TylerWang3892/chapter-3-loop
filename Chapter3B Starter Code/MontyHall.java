c81796b4ce0437d1ec9716bbedff657bee3188cf
TylerWang3892 <54153292+TylerWang3892@users.noreply.github.com>
2019-10-09 14:12:53 -0500

    finished monty hall

--- Chapter3B Starter Code/MontyHall.java
+++ Chapter3B Starter Code/MontyHall.java
@@ -1,13 +1,27 @@
-import java.util.Scanner;
 import java.util.Random;
 
 public class MontyHall
 {
     public static void main()
     {
+        Random gen = new Random();
         int strat1 = 0, strat2 = 0;
-        int door;
-        for(int a = 0; a < 1000; a++)
+        int ansDoor, pickDoor;
         
+        //stay strat
+        for(int a = 0; a < 1000; a++)
+        {
+            if(gen.nextInt(3) == gen.nextInt(3))
+                strat1++;
+        }
+        
+        //switch strat
+        for(int a = 0; a < 1000; a++)
+        {
+            if(gen.nextInt(3) != gen.nextInt(3))
+                strat2++;
+        }
+        System.out.println();
+        System.out.println("Strategy 1: " + strat1 + "\nStrategy 2: " + strat2);
     }
 }
