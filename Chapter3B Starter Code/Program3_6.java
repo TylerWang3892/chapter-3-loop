e4501b79fdf6b3be212a7595f7176ea78acfbe36
TylerWang3892 <54153292+TylerWang3892@users.noreply.github.com>
2019-10-09 14:25:17 -0500

    d

---
+++ Chapter3B Starter Code/Program3_6.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Program3_6
+{
+    public static void main()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String largeNumber = keyboard.nextLine();
+        
+        for(int a = 0; a < largeNumber.length(); a++)
+            
+    }
+}
