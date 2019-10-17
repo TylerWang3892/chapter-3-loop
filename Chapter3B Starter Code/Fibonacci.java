1b5cefecedb9bf971a9f647a37c99555c6284f71
TylerWang3892 <54153292+TylerWang3892@users.noreply.github.com>
2019-10-17 14:14:16 -0500

    sdf

--- Chapter3B Starter Code/Fibonacci.java
+++ Chapter3B Starter Code/Fibonacci.java
@@ -7,15 +7,21 @@ public class Fibonacci
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the amount of digits you want to output.");
         int previous = 1, twoBefore = 0, buffer = 0;
-        int a = keyboard.nextInt() - 2;
-        System.out.print(twoBefore + " ");
-        System.out.print(previous + " ");
-        for(a = a; a > 0; a--)
+        int numofNumbers = keyboard.nextInt();
+        for(int a = 0; numofNumbers > a; a++)
         {
-            System.out.print(twoBefore + previous + " ");
-            buffer = twoBefore;
-            twoBefore = previous;
-            previous = buffer + twoBefore;
+            if(a ==0)
+                System.out.print(twoBefore + " ");
+
+            else if(a == 1)
+                System.out.print(previous + " ");
+            else
+            {
+                System.out.print(twoBefore + previous + " ");
+                buffer = twoBefore;
+                twoBefore = previous;
+                previous = buffer + twoBefore;
+            }
         }
     }
 }
--- Chapter3B Starter Code/Palendrome.java
+++ Chapter3B Starter Code/Palendrome.java
@@ -8,22 +8,28 @@ public class Palendrome
         System.out.println("Enter a phase all in one line");
         String phrase = keyboard.nextLine();
         System.out.println();
+        
         boolean pallendrome = true;
-        String part1, part2;
+        String part1 = "d", part2 = "d";
+        System.out.print(phrase);
+        
         while(phrase.indexOf(' ') != -1)
         {
-            part1 = phrase.substring(0,phrase.indexOf(' '));
+            part1 = phrase.substring(0 , phrase.indexOf(' '));
             part2 = phrase.substring(phrase.indexOf(' '));
-            phrase = part1 + part2;
+            //phrase = part1 + part2;
         }
         
-        for(int a = 0; a < phrase.length(); a++)
-            if(phrase.charAt(a) != phrase.charAt(phrase.length() - a))
+        for(int a = 0; a <= phrase.length()/2; a++)
+            if(phrase.charAt(a) != phrase.charAt((phrase.length() - 1) - a))
             {
                 pallendrome = false;
                 break;
             }
             
         if(pallendrome)
+            System.out.println(" is a pallendrome");
+        else
+            System.out.println(" isn't a pallendrome");
     }
 }
